d978eb5f5dcd7f4d3cd60572cd1dac6de5d81eed
Sander De Vos <dvsander@users.noreply.github.com>
2023-08-13 15:40:53 +0200

    update project structure

--- api/app.py
+++ api/app.py
@@ -81,5 +81,3 @@ def hello_world():
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
