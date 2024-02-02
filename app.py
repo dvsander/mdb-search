900e96192575b354441c164318e0f40dddc21934
Sylvain Chambon <sylvain.chambon@mongodb.com>
2024-02-02 17:41:51 +0100

    Add ability to create new movies. It's expected to have a trigger to add the plot embeddings

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 import os
-from flask import Flask, render_template, send_from_directory, request
+from flask import Flask, render_template, send_from_directory, request, redirect
 from bson.objectid import ObjectId
 from dotenv import load_dotenv
 load_dotenv()
 
 from database import getCollection
 from controller import getOpenAIEmbedding
+import requests
+import base64
+from sentence_transformers import SentenceTransformer
+from PIL import Image, ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+from io import BytesIO
 
 app = Flask(__name__)
+
+model = SentenceTransformer('clip-ViT-B-32')
 
 @app.route("/search", methods=['GET', 'POST'])
 def search():
@@ -48,6 +56,34 @@ def search():
         ])
 
     return render_template("home.html",movies=docs, searchInput=searchInput, searchOptions=searchOptions)
+
+@app.route("/new", methods=["POST"])
+def new():
+    title = request.form["title"]
+    plot = request.form["plot"]
+    poster = request.form["poster"]
+
+    poster_b64 = downloadImageAsBase64(poster)
+    poster_embedding = model.encode(Image.open(BytesIO(base64.b64decode(poster_b64))), convert_to_numpy=True)
+
+    doc = {
+        "title": title,
+        "plot": plot,
+        "demoAdded": True,
+        "poster_blob": poster_b64,
+        "poster_embedding": poster_embedding.tolist()
+    }
+
+    getCollection().insert_one(doc)
+    
+    return redirect("/", code=302)
+
+def downloadImageAsBase64(url):
+   headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
+   httpRequest = requests.get(url,headers=headers)
+   if (httpRequest.status_code == 200):
+       return base64.b64encode(httpRequest.content).decode('ascii')
+   return None
 
 @app.route("/similarImage/<movieId>")
 def findSimilarPostersTos(movieId):
